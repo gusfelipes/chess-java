ced6b797df7707ea7642d4c12ca70d201864d659
gusfelipes <gusfelipes@gmail.com>
2020-09-26 20:47:02 -0300

    BoardException and defensive programming

--- src/boardgame/Board.java
+++ src/boardgame/Board.java
@@ -2,43 +2,61 @@ package boardgame;
 
 public class Board {
 
-	private int row;
-	private int column;
+	private int rows;
+	private int columns;
 	private Piece[][] pieces;
 
 	public Board(int row, int column) {
-		this.row = row;
-		this.column = column;
+		if (row < 1 || column < 1) {
+			throw new BoardException("Error creating board: There must be at least 1 row and 1 column!");
+		}
+		this.rows = row;
+		this.columns = column;
 		this.pieces = new Piece[row][column];
 	}
 
-	public int getRow() {
-		return row;
+	public int getRows() {
+		return rows;
 	}
 
-	public void setRow(int row) {
-		this.row = row;
+	public int getColumns() {
+		return columns;
 	}
 
-	public int getColumn() {
-		return column;
-	}
-
-	public void setColumn(int column) {
-		this.column = column;
-	}
-	
 	public Piece piece(int row, int column) {
+		if (!positionExists(row, column)) {
+			throw new BoardException("Position not in the board!");
+		}
 		return pieces[row][column];
 	}
-	
+
 	public Piece piece(Position position) {
+		if (!positionExists(position)) {
+			throw new BoardException("Position not in the board!");
+		}
 		return pieces[position.getRow()][position.getColumn()];
 	}
-	
+
 	public void placePiece(Piece piece, Position position) {
+		if (thereIsAPiece(position)) {
+			throw new BoardException("There is already a piece in position: " + position);
+		}
 		pieces[position.getRow()][position.getColumn()] = piece;
 		piece.position = position;
 	}
 
+	private boolean positionExists(int row, int column) {
+		return row >= 0 && row < rows && column >= 0 && column < columns;
+	}
+
+	public boolean positionExists(Position position) {
+		return positionExists(position.getRow(), position.getColumn());
+	}
+
+	public boolean thereIsAPiece(Position position) {
+		if (!positionExists(position)) {
+			throw new BoardException("Position not in the board!");
+		}
+		return piece(position) != null;
+	}
 }
--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -15,9 +15,9 @@ public class ChessMatch {
 	}
 
 	public ChessPiece[][] getPieces() {
-		ChessPiece[][] mat = new ChessPiece[board.getRow()][board.getColumn()];
-		for (int i = 0; i < board.getRow(); i++) {
-			for (int j = 0; j < board.getColumn(); j++) {
+		ChessPiece[][] mat = new ChessPiece[board.getRows()][board.getColumns()];
+		for (int i = 0; i < board.getRows(); i++) {
+			for (int j = 0; j < board.getColumns(); j++) {
 				mat[i][j] = (ChessPiece) board.piece(i, j);
 
 			}
@@ -28,6 +28,7 @@ public class ChessMatch {
 	public void initialSetup() {
 		board.placePiece(new Rook(board, Color.WHITE), new Position(2, 1));
 		board.placePiece(new King(board, Color.BLACK), new Position(0,	4));
+		board.placePiece(new King(board, Color.BLACK), new Position(7,	4));
 	}
 
 }
